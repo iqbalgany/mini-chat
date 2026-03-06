0f8618d29c5293d28dc7e3b0fa8a7cd41579e7ca
Muhammad Iqbal Al Afgany <rismagany1203@gmail.com>
2026-03-06 08:26:14 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:mini_chat/presentations/pages/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const HomePage());
+  }
+}
